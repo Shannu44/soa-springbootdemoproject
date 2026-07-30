71b7b6367eb8826196200894e8f6908f0550d57a
Shannu44 <2400030139@kluniversity.in>
2026-07-30 17:15:20 +0530

    Implemented RestTemplate

---
+++ src/main/java/com/klef/soa/controller/APIController.java
@@ -0,0 +1,33 @@
+package com.klef.soa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.klef.soa.dto.ProductDTO;
+import com.klef.soa.service.APIService;
+
+@RestController
+@RequestMapping("Product")
+public class APIController 
+{
+    @Autowired
+	private APIService  service;
+	
+    @GetMapping("/displayall")
+    public List<ProductDTO> displayProductbyid(int id)
+    {
+    	return service.displayAllProducts();
+    }
+    
+    @GetMapping("display/{id}")
+    public ProductDTO displayProductbyid1(@PathVariable int id) 
+    {
+    	return service.displayProductById();
+    }
+    
+}
